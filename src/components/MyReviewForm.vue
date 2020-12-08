04e80e38177648c50d644670e74cd841a7634f3d
KaimNotark <kaimnotark@yandex.ru>
2020-12-08 09:44:44 +0800

    add btn and modal window

---
+++ src/components/MyReviewForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis mollitia
+      magnam quam itaque tenetur temporibus asperiores molestiae consequuntur
+      dolores laboriosam? Ipsam eligendi quod veritatis qui, corporis natus ea
+      nesciunt maxime.
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: "MyReviewForm",
+};
+</script>
+<style lang="scss">
+@import "../stylesheets/variables.scss";
+</style>
